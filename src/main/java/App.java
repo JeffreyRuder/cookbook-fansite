f5023e35fdbcb42ec2cbc54769c49ba3781c0994
Jeffrey Ruder <ruderjt@gmail.com>
2015-12-18 12:27:49 -0800

    Add forms to Contact page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -28,5 +28,34 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/receipt", (request, response) -> {
+      HashMap model = new HashMap();
+      String title = request.queryParams("title");
+      String author = request.queryParams("author");
+
+      model.put("title", title);
+      model.put("author", author);
+      model.put("template", "templates/receipt.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/receipt2", (request, response) -> {
+      HashMap model = new HashMap();
+      String rating = request.queryParams("rating");
+
+      model.put("rating", rating);
+      model.put("template", "templates/receipt2.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/receipt3", (request, response) -> {
+      HashMap model = new HashMap();
+      String email = request.queryParams("email");
+
+      model.put("email", email);
+      model.put("template", "templates/receipt3.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
   }
 }
